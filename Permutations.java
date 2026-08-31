437956c047629535cc184f760d41329adfb0e3a2
Deepak Soni <deepaksonii@outlook.in>
2026-08-31 16:25:00 +0530

    solved Permutations

--- Permutations.java
+++ Permutations.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Permutations {
     public static void main(String[] args) {
-        //
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println(n);
+
+            sc.close();
+            System.exit(0);
+        }
+
+        if (n == 4) {
+            System.out.println("2 4 1 3");
+
+            sc.close();
+            System.exit(0);
+        }
+
+        if (n < 5) {
+            System.out.println("NO SOLUTION");
+
+            sc.close();
+            System.exit(0);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i += 2) {
+            sb.append(i + " ");
+        }
+
+        for (int i = 2; i <= n; i += 2) {
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb.toString());
+
+        sc.close();
     }
 }
