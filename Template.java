e2de39f73e7ab19d19b9fbc583e9b57538b601e7
Deepak Soni <deepaksonii@outlook.in>
2026-09-01 14:13:59 +0530

    add Template.java

---
+++ Template.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Template {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //
+
+        sc.close();
+    }
+
+    private static void print(Object o) {
+        System.out.print(o);
+    }
+
+    private static void println(Object o) {
+        System.out.println(o);
+    }
+}
